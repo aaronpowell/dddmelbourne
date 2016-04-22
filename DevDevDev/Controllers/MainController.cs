b6f90d800792511b6cdc27576198b439b9f73452
Aaron Powell <me@aaron-powell.com>
2016-04-23 07:05:31 +1000

    Fixing mobile

--- DevDevDev/Controllers/MainController.cs
+++ DevDevDev/Controllers/MainController.cs
@@ -65,10 +65,10 @@ namespace DevDevDev.Controllers
             return RedirectToAction("SubmitError");
         }
 
-        /*public ActionResult Register()
+        public ActionResult Register()
         {
-            return new RedirectResult("https://www.eventbrite.com.au/e/ddd-melbourne-6-2015-registration-16657196122/");
-        }*/
+            return new RedirectResult("https://www.eventbrite.com.au/e/ddd-sydney-tickets-24335111975");
+        }
 
         public ActionResult SessionSubmittedSuccess()
         {
