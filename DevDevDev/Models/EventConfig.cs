a42135a98b13fa23eb6c332ace3c01520ba68f06
Aaron Powell <me@aaron-powell.com>
2017-04-28 10:05:34 +1000

    dates

--- DevDevDev/Models/EventConfig.cs
+++ DevDevDev/Models/EventConfig.cs
@@ -3,7 +3,7 @@
     public static class EventConfig
     {
         public const string EventTitle = "DDD Sydney";
-        public const string EventDateDescription = "July 2017";
+        public const string EventDateDescription = "15th July 2017";
         public const int TotalVotes = 10;
     }
 }
