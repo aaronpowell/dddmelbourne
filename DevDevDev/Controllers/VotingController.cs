551a14441763c8348a65a25eb73cc55650b50c10
Aaron Powell <me@aaron-powell.com>
2017-06-05 09:05:28 +1000

    Turning on voting

--- DevDevDev/Controllers/VotingController.cs
+++ DevDevDev/Controllers/VotingController.cs
@@ -1,58 +1,58 @@
-﻿//using System.Collections.Generic;
-//using System.Net;
-//using System.Web.Mvc;
-//using DevDevDev.Infrastructure;
-//using DevDevDev.Models;
-//using DevDevDev.Services;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Web.Mvc;
+using DevDevDev.Infrastructure;
+using DevDevDev.Models;
+using DevDevDev.Services;
 
-//namespace DevDevDev.Controllers
-//{
-//    public class VotingController : Controller
-//    {
-//        private readonly VoteService _voteService = new VoteService();
-//        private readonly SubmittedSessionsService _submittedSessionsService = new SubmittedSessionsService();
+namespace DevDevDev.Controllers
+{
+    public class VotingController : Controller
+    {
+        private readonly VoteService _voteService = new VoteService();
+        private readonly SubmittedSessionsService _submittedSessionsService = new SubmittedSessionsService();
 
-//        public ActionResult SessionsToVoteFor()
-//        {
-//            return View();
-//        }
+        public ActionResult SessionsToVoteFor()
+        {
+            return View();
+        }
 
-//        [OutputCache(Duration = 3600)]
-//        public ActionResult GetSessionsAsJson()
-//        {
-//            var model = _submittedSessionsService.GetSessionsForVoting();
+        [OutputCache(Duration = 3600)]
+        public ActionResult GetSessionsAsJson()
+        {
+            var model = _submittedSessionsService.GetSessionsForVoting();
 
-//            return Json(model, JsonRequestBehavior.AllowGet);
-//        }
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
 
-//        [Throttle(Name = "ThrottleVote", Message = "Request throttled", Seconds = 5)]
-//        [HttpPost]
-//        public ActionResult SubmitVote(string[] sessionIds)
-//        {
-//            if (sessionIds.Length != EventConfig.TotalVotes)
-//            {
-//                return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
-//            }
+        [Throttle(Name = "ThrottleVote", Message = "Request throttled", Seconds = 5)]
+        [HttpPost]
+        public ActionResult SubmitVote(string[] sessionIds)
+        {
+            if (sessionIds.Length != EventConfig.TotalVotes)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+            }
 
-//            var votes = new List<Vote>();
-//            for (var i = 0; i < EventConfig.TotalVotes; i++)
-//            {
-//                votes.Add(new Vote(sessionIds[i], System.Web.HttpContext.Current.Request.UserHostAddress));
-//            }
+            var votes = new List<Vote>();
+            for (var i = 0; i < EventConfig.TotalVotes; i++)
+            {
+                votes.Add(new Vote(sessionIds[i], System.Web.HttpContext.Current.Request.UserHostAddress));
+            }
 
-//            _voteService.AddVotes(votes);
+            _voteService.AddVotes(votes);
 
-//            return new HttpStatusCodeResult(HttpStatusCode.OK);
-//        }
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
 
-//        public ActionResult VotedSuccessfully()
-//        {
-//            return View();
-//        }
+        public ActionResult VotedSuccessfully()
+        {
+            return View();
+        }
 
-//        public ActionResult VotingFailure()
-//        {
-//            return View();
-//        }
-//    }
-//}
+        public ActionResult VotingFailure()
+        {
+            return View();
+        }
+    }
+}
