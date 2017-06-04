64750578ce87f05611ff6a75b1d276d18434e247
Aaron Powell <me@aaron-powell.com>
2017-06-05 09:14:28 +1000

    Randomising the session list

--- DevDevDev/Services/SubmittedSessionsService.cs
+++ DevDevDev/Services/SubmittedSessionsService.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
-using DevDevDev.Infrastructure;
+﻿using DevDevDev.Infrastructure;
 using DevDevDev.Models;
 using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DevDevDev.Services
 {
@@ -9,12 +11,12 @@ namespace DevDevDev.Services
     {
 
         private const string _sessionsTableName = "Sessions";
+        static Random random = new Random();
 
         public SubmittedSessionsService()
             : base(_sessionsTableName)
         {
         }
-
 
         public VotingSessionsViewModel GetSessionsForVoting()
         {
@@ -28,19 +30,21 @@ namespace DevDevDev.Services
 
             var sessions = sessionTable.ExecuteQuery(sessionQuery).ToList();
 
+            sessions = RandomPermutation(sessions).ToList();
+
             foreach (var sessionSubmission in sessions)
             {
-                var session = new VotingSubmissionViewModel();
-
-                session.SessionId = sessionSubmission.RowKey;
-                session.SessionTitle = sessionSubmission.SessionTitle;
-                session.SessionAbstract = sessionSubmission.SessionAbstract.FormatForHtml();
-                session.PresenterName = sessionSubmission.PresenterName;
-                session.PresenterTwitterAlias = sessionSubmission.PresenterTwitterAlias;
-                session.PresenterWebsite = sessionSubmission.PresenterWebsite;
-                session.PresenterBio = sessionSubmission.PresenterBio;
-                session.RecommendedAudience = sessionSubmission.RecommendedAudience;
-
+                var session = new VotingSubmissionViewModel()
+                {
+                    SessionId = sessionSubmission.RowKey,
+                    SessionTitle = sessionSubmission.SessionTitle,
+                    SessionAbstract = sessionSubmission.SessionAbstract.FormatForHtml(),
+                    PresenterName = sessionSubmission.PresenterName,
+                    PresenterTwitterAlias = sessionSubmission.PresenterTwitterAlias,
+                    PresenterWebsite = sessionSubmission.PresenterWebsite,
+                    PresenterBio = sessionSubmission.PresenterBio,
+                    RecommendedAudience = sessionSubmission.RecommendedAudience
+                };
                 viewModel.SessionsToVoteFor.Add(session);
 
             }
@@ -52,9 +56,9 @@ namespace DevDevDev.Services
             var tableClient = GetTableClient();
             var sessionTable = tableClient.GetTableReference(_sessionsTableName);
             var sessionQuery = (new TableQuery<SessionSubmission>()).Where(TableQuery.GenerateFilterConditionForInt("Status", QueryComparisons.Equal, 1));
-         
+
             var sessions = sessionTable.ExecuteQuery(sessionQuery).ToList();
-            
+
             var viewModel = new SubmittedSessionsViewModel();
 
             foreach (var sessionSubmission in sessions)
@@ -74,6 +78,24 @@ namespace DevDevDev.Services
 
             }
             return viewModel;
+        }
+
+        static IEnumerable<T> RandomPermutation<T>(IEnumerable<T> sequence)
+        {
+            var retArray = sequence.ToArray();
+
+            for (var i = 0; i < retArray.Length - 1; i += 1)
+            {
+                var swapIndex = random.Next(i, retArray.Length);
+                if (swapIndex != i)
+                {
+                    var temp = retArray[i];
+                    retArray[i] = retArray[swapIndex];
+                    retArray[swapIndex] = temp;
+                }
+            }
+
+            return retArray;
         }
     }
 }
